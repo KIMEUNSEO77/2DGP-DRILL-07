56c8b37ba86f5a0c8a669c2569adacc0986b680d
eunseo <2023180012@tukorea.ac.kr>
2025-09-30 21:33:52 +0900

    Ball 클래스 생성

--- ball_game.py
+++ ball_game.py
@@ -1,6 +1,13 @@
 from pico2d import *
 import random
 
+class Ball:
+    def __init__(self):
+        self.x = random.randint(0, 800)
+        self.y = 599
+        self.small_image = load_image('ball21x21.png')
+        self.big_image = load_image('ball41x41.png')
+
 open_canvas()
 
 close_canvas()
